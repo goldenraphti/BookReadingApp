46209e702ca9e4d5ea3ad5e5f770f0c15dac138f
Raphaël Ferrand <ferrand@protonmail.ch>
2018-07-10 17:35:06 +0200

    feat: fetch books from API and display it on corresponding shelves

--- src/App.js
+++ src/App.js
@@ -1,35 +1,43 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
-import './App.css'
 import ListBooks from './ListBooks'
 import SearchPage from './SearchPage'
+import './App.css'
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
+    
+    state= {
+        books:[],
+    }
 
-  render() {
-    return (
-      <div className="app">
+    componentDidMount() {
+        BooksAPI.getAll().then(books => {
+                this.setState({books:books});
+            })
+    }
+    
+    updateBook = (book, shelf) => {
+        BooksAPI.update(book, shelf)
+    }
 
-             <Route exact  path="/" render={() => (
-                <ListBooks />
-            )} />
+    render() {
+        return (
+          <div className="app">
 
-            <Route path="/search" render={() => (
-                <SearchPage />
-            )} />
-      </div>
-    )
-  }
+                 <Route exact  path="/" render={() => (
+                    <ListBooks 
+                        onUpdateBook={this.updateBook}
+                        books={this.state.books}
+                    />
+                )} />
+
+                <Route path="/search" render={() => (
+                    <SearchPage />
+                )} />
+          </div>
+        )
+    }
 }
 
 export default BooksApp
