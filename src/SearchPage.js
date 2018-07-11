43ee6502bcf1bca71fff6ff1216b42415fbf5d83
Raphaël Ferrand <ferrand@protonmail.ch>
2018-07-11 17:09:31 +0200

    feat: deleting queries or non-matching searches clear the book grid display, and non-matching suggest list of search terms if search query does not match any term

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -9,19 +9,27 @@ class SearchPage extends Component {
     state = {
         books:[],
         query: '',
-        searchTerms: ['Android', 'Art', 'Artificial Intelligence', 'Astronomy', 'Austen', 'Baseball', 'Basketball', 'Bhagat', 'Biography', 'Brief', 'Business', 'Camus', 'Cervantes', 'Christie', 'Classics', 'Comics', 'Cook', 'Cricket', 'Cycling', 'Desai', 'Design', 'Development', 'Digital Marketing', 'Drama', 'Drawing', 'Dumas', 'Education', 'Everything', 'Fantasy', 'Film', 'Finance', 'First', 'Fitness', 'Football', 'Future', 'Games', 'Gandhi', 'Homer', 'Horror', 'Hugo', 'Ibsen', 'Journey', 'Kafka', 'King', 'Lahiri', 'Larsson', 'Learn', 'Literary Fiction', 'Make', 'Manage', 'Marquez', 'Money', 'Mystery', 'Negotiate', 'Painting', 'Philosophy', 'Photography', 'Poetry', 'Production', 'Programming', 'React', 'Redux', 'River', 'Robotics', 'Rowling', 'Satire', 'Science Fiction', 'Shakespeare', 'Singh', 'Swimming', 'Tale', 'Thrun', 'Time', 'Tolstoy', 'Travel', 'Ultimate', 'Virtual Reality', 'Web Development', 'iOS']
+        queryCondition:'',
+        searchTerms: ['Android', 'Art', 'Artificial Intelligence', 'Astronomy', 'Austen', 'Baseball', 'Basketball', 'Bhagat', 'Biography', 'Brief', 'Business', 'Camus', 'Cervantes', 'Christie', 'Classics', 'Comics', 'Cook', 'Cricket', 'Cycling', 'Desai', 'Design', 'Development', 'Digital Marketing', 'Drama', 'Drawing', 'Dumas', 'Education', 'Everything', 'Fantasy', 'Film', 'Finance', 'First', 'Fitness', 'Football', 'Future', 'Games', 'Gandhi', 'Homer', 'Horror', 'Hugo', 'Ibsen', 'Journey', 'Kafka', 'King', 'Lahiri', 'Larsson', 'Learn', 'Literary Fiction', 'Make', 'Manage', 'Marquez', 'Money', 'Mystery', 'Negotiate', 'Painting', 'Philosophy', 'Photography', 'Poetry', 'Production', 'Programming', 'React', 'Redux', 'River', 'Robotics', 'Rowling', 'Satire', 'Science Fiction', 'Shakespeare', 'Singh', 'Swimming', 'Tale', 'Thrun', 'Time', 'Tolstoy', 'Travel', 'Ultimate', 'Virtual Reality', 'Web Development', 'iOS'],
     }
     
     filterQuery = (query) => {
         
         const match = new RegExp(escapeRegExp(query), 'i')
   
-        if (match.test(this.state.searchTerms) ) {
+        if (query === '') {
+            console.log('query is empty')
+            this.setState({queryCondition:'empty'})
+        } else if (match.test(this.state.searchTerms) ) {
             this.updateQuery(query)
+            this.setState({queryCondition:'validSearch'})
         } else {
-            console.log('query not matching any allowed topic');
+            console.log('query not matching any allowed topic')
+            this.setState({queryCondition:'error'})
+            
         }
     }
+
 
     updateQuery = (query) => {
         
@@ -39,6 +47,12 @@ class SearchPage extends Component {
         
                 
         const booksList = this.state.books;
+        
+        const searchTermsStyle = {
+            color: 'grey',
+            fontSize: '0.9em',
+            fontStyle: 'italic'
+        }
     
         return (
             
@@ -55,16 +69,29 @@ class SearchPage extends Component {
                     </div>
                 </div>
                 <div className="search-books-results">
-                    <ol className="books-grid">
+                   
+                    {(this.state.queryCondition === 'validSearch' && (
 
-                        {booksList.map(book => (
+                        <ol className="books-grid">
+                            {booksList.map(book => (
                             <BookDisplay
                                 bookToDisplay={book}
                                 onUpdateBook={this.props.onUpdateBook}
                             />
+                            ))}
+                        </ol> 
                         ))}
-
-                    </ol>
+                        
+                    {(this.state.queryCondition === 'error' && (
+                        <p>Your search does not return any result from our catalog. Please use one of the following search terms:
+                            <span style={searchTermsStyle}>
+                                {this.state.searchTerms.map((term) => (
+                                    <span> {term},</span>
+                                ))}
+                            </span>
+                        </p>
+                    ))}
+                    
                 </div>
             </div>
             
