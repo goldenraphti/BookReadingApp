03df4ab02afaa68dda4c8d9c3330aa98924067ea
Raphaël Ferrand <ferrand@protonmail.ch>
2018-07-11 14:22:24 +0200

    fix: if query in SearchPage does not match any accepted term, it does not break
    using filterQuery method to test regexp of the query against the this.state.searchTerms

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -12,14 +12,27 @@ class SearchPage extends Component {
         searchTerms: ['Android', 'Art', 'Artificial Intelligence', 'Astronomy', 'Austen', 'Baseball', 'Basketball', 'Bhagat', 'Biography', 'Brief', 'Business', 'Camus', 'Cervantes', 'Christie', 'Classics', 'Comics', 'Cook', 'Cricket', 'Cycling', 'Desai', 'Design', 'Development', 'Digital Marketing', 'Drama', 'Drawing', 'Dumas', 'Education', 'Everything', 'Fantasy', 'Film', 'Finance', 'First', 'Fitness', 'Football', 'Future', 'Games', 'Gandhi', 'Homer', 'Horror', 'Hugo', 'Ibsen', 'Journey', 'Kafka', 'King', 'Lahiri', 'Larsson', 'Learn', 'Literary Fiction', 'Make', 'Manage', 'Marquez', 'Money', 'Mystery', 'Negotiate', 'Painting', 'Philosophy', 'Photography', 'Poetry', 'Production', 'Programming', 'React', 'Redux', 'River', 'Robotics', 'Rowling', 'Satire', 'Science Fiction', 'Shakespeare', 'Singh', 'Swimming', 'Tale', 'Thrun', 'Time', 'Tolstoy', 'Travel', 'Ultimate', 'Virtual Reality', 'Web Development', 'iOS']
     }
     
+    filterQuery = (query) => {
+        
+        const match = new RegExp(escapeRegExp(query), 'i')
+  
+        if (match.test(this.state.searchTerms) ) {
+            this.updateQuery(query)
+        } else {
+            console.log('query not matching any allowed topic');
+        }
+    }
+
     updateQuery = (query) => {
+        
+        console.log(query);
+        
         BooksAPI.search(query).then(books => {
             if(books) {
                 this.setState({ books }) 
             }
         })
         this.setState({ query });
-//        console.log(this.state.books)
     }
     
     render() {
@@ -36,7 +49,7 @@ class SearchPage extends Component {
                        <input
                            type='text'
                            placeholder='Search by title or author'
-                           onChange={ (event) => this.updateQuery(event.target.value) }    
+                           onChange={ (event) => this.filterQuery(event.target.value) }    
                        />
 
                     </div>
